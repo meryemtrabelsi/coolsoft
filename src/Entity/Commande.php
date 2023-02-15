cd4bfc2e0a2ce4ec06e26919eb3daf0912b8078f
farouk223 <farouk.elelayech@esprit.tn>
2023-02-15 23:43:49 +0100

    crud 50%

--- src/Entity/Commande.php
+++ src/Entity/Commande.php
@@ -6,6 +6,7 @@ use App\Repository\CommandeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommandeRepository::class)]
 class Commande
@@ -16,6 +17,7 @@ class Commande
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"farouk")]
     private ?string $statue = null;
 
     #[ORM\Column(length: 255)]
